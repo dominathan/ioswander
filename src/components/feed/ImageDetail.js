82b5296445d22b3c8292dc24893909d1b08f068e
Nathan Hall <nathan.mh@gmail.com>
2017-04-16 12:43:49 -0400

    Style tweaks.

--- src/components/feed/ImageDetail.js
+++ src/components/feed/ImageDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { Tile } from 'react-native-elements';
+import { Actions } from 'react-native-router-flux';
 
 export class ImageDetail extends Component {
 
@@ -18,12 +19,14 @@ export class ImageDetail extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#222'
     },
     imageContainer: {
       flex: 3,
       width: null,
       height: null,
-      resizeMode: 'contain'
+      resizeMode: 'contain',
+      backgroundColor: '#222'
     }
 });
